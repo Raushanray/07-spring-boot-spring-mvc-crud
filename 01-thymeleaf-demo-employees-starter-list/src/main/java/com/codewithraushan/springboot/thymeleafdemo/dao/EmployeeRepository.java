39b12c5e11f9d140c1a6b9e6e110dd88e56cdafc
Raushanray <raushankuray1998@gmail.com>
2023-07-05 11:34:37 +0530

    create a spring boot project using thymeleaf for preforming the CRUD operation without database

---
+++ 01-thymeleaf-demo-employees-starter-list/src/main/java/com/codewithraushan/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.codewithraushan.springboot.thymeleafdemo.dao;
+
+import com.codewithraushan.springboot.thymeleafdemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
+	
+}
